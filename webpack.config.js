b5604f67502c5dadfee902fa106f2003af05aa5b
lukebelliveau <lbellive@thoughtworks.com>
2017-06-07 16:12:30 -0500

    Hello World with webpack/babel setup plus linter + devserver

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+module.exports = {
+  entry: './src/App.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'app.bundle.js',
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/ },
+      { test: /\.jsx$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/ }
+    ]
+  },
+};
