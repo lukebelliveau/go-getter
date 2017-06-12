bf1b131d5eda7453aa3d1361915b15c7a0f3374b
lukebelliveau <lbellive@thoughtworks.com>
2017-06-12 17:56:32 -0500

    responsive toast message

--- src/SubmitBrand.js
+++ src/SubmitBrand.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import update from 'immutability-helper';
 import { Snackbar } from 'material-ui';
 import BrandDialog from './BrandDialog';
+import ResponsiveContainer from './ResponsiveContainer';
 import { registerBrandInCity } from './api/apiHelper'
 
 const toastDuration = 4000;
+
+//bad for maintainability -- need to figure out better place to hold messages
+const messageColor = (message) => message.includes('Congrats') ? '#388E3C' : '#D50000';
 
 class SubmitBrand extends Component {
   constructor(props) {
@@ -87,18 +91,39 @@ class SubmitBrand extends Component {
   }
 }
 
-const Toast = ({ open, message }) => (
-  <Snackbar
-    open={ open }
-    message={ message }
-    autoHideDuration={ toastDuration }
-    bodyStyle={{
-      backgroundColor: messageColor(message)
-    }}
-  />
-);
+const Toast = ({ open, message }) => {
+  const {
+    body,
+    style,
+    content
+  } = mobileStyles(message);
 
-//bad for maintainability -- need to figure out better place to hold messages
-const messageColor = (message) => message.includes('Congrats') ? '#388E3C' : '#D50000';
+  return ResponsiveContainer({
+    mobileComponent: <Snackbar open={ open }message={ message }
+                        autoHideDuration={ toastDuration } bodyStyle={ body }
+                        style={ style } contentStyle={ content } />,
+    desktopComponent: <Snackbar open={ open } message={ message }
+                        autoHideDuration={ toastDuration }
+                        bodyStyle={{ backgroundColor: messageColor(message) }} />
+  })
+};
+
+const mobileStyles = (message) => ({
+  body: {
+    backgroundColor: messageColor(message),
+    height: 200,
+    maxWidth: 'none',
+  },
+  style: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  content: {
+    fontSize: 50,
+    textAlign: 'center',
+    marginTop: 50,
+  },
+});
 
 export default SubmitBrand;
