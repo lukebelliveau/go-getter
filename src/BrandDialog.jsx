f29d2bc3eedb73b6d4a5173ead3fbbfee4347abe
lukebelliveau <lbellive@thoughtworks.com>
2017-06-16 15:57:14 -0400

    finished a really ugly draft of mobile dialog, gonna combine the 2

--- src/BrandDialog.jsx
+++ src/BrandDialog.jsx
@@ -29,9 +29,10 @@ class Modal extends React.Component {
   render() {
     return (
       this.props.show
-        ? <Overlay onClick={ this.click } id="overlay">
+        ?
+        <Overlay onClick={ this.click } id="overlay">
             <Container>
-              <Header>{ this.props.brand }</Header>
+              <Header>A Header Brand</Header>
               <Body onChange={ this.props.onChangeCity }>{ this.props.city }</Body>
               <Footer/>
             </Container>
@@ -53,13 +54,27 @@ const fadeIn = keyframes`
   }
 `;
 
-const Overlay = styled.div`
+const OverlayDesktop = styled.div`
   animation: ${fadeIn} 0.2s linear;
   display: block;
-  // display: none; /* Hidden by default */
   position: fixed; /* Stay in place */
   z-index: 1; /* Sit on top */
   padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  // overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+`;
+
+const Overlay = styled.div`
+  animation: ${fadeIn} 0.2s linear;
+  display: block;
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 25%; /* Location of the box */
   left: 0;
   top: 0;
   width: 100%; /* Full width */
@@ -70,6 +85,89 @@ const Overlay = styled.div`
 `;
 
 const Container = styled.div`
+  position: relative;
+  background-color: #fefefe;
+  margin: auto;
+  padding: 0;
+  width: 80%;
+  height: 50%;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
+  -webkit-animation-name: animatetop;
+  -webkit-animation-duration: 0.4s;
+  animation-name: animatetop;
+  animation-duration: 0.4s
+  &:after {
+    content:""; 
+    background: black; 
+    position: absolute; 
+    bottom: 0; 
+    left: 0; 
+    height: 50%; 
+    width: 1px;
+  }
+`;
+const HeaderContainer = styled.div`
+  padding: 2px 16px;
+  font-family: sans-serif;
+  font-size: 40px;
+  &:after {
+    content:''; 
+    width: 90%; 
+    height:1px; 
+    background: lightgray; 
+    position:absolute; 
+  }
+`;
+const BodyContainer = styled.div`
+  padding: 20px 16px 0px 16px;
+  height: 40%;
+`;
+const Input = styled.input`
+  font-size: 40px;
+  margin: 40px 25px;
+  width: 90%;
+  display: block;
+  border: none;
+  padding: 10px 0;
+  border-bottom: solid 1px ${primaryColor};
+  -webkit-transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
+  transition: all 0.3s cubic-bezier(0.64, 0.09, 0.08, 1);
+  background: -webkit-linear-gradient(top, rgba(255, 255, 255, 0) 96%, ${primaryColor} 4%);
+  background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 96%, ${primaryColor} 4%);
+  background-position: -200px 0;
+  background-size: 200px 100%;
+  background-repeat: no-repeat;
+  color: #0e6252;
+  font-family: 'roboto', sans-serif;
+  -webkit-transition: all 0.3s ease-in-out;
+          transition: all 0.3s ease-in-out;
+  &:focus{
+    box-shadow: none;
+    outline: none;
+    font-size: 40px;
+  }
+`;
+
+const FooterContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  padding: 2px 16px;
+  color: white;
+  width: 90%;
+`;
+const Button = styled.div`
+  margin: 5px;
+  padding: 10px;
+  color: ${props => props.color};
+  font-family: Roboto, sans-serif;
+  font-size: 50px;
+  &:hover{
+    background-color: #EEEEEE;
+    cursor: pointer;
+  }
+`;
+
+const ContainerDesktop = styled.div`
   position: relative;
   background-color: #fefefe;
   margin: auto;
@@ -91,7 +189,7 @@ const Container = styled.div`
   }
 `;
 
-const HeaderContainer = styled.div`
+const HeaderContainerDesktop = styled.div`
   padding: 2px 16px;
   font-family: sans-serif;
   &:after {
@@ -108,11 +206,11 @@ const Header = ({ children }) => (
   </HeaderContainer>
 );
 
-const BodyContainer = styled.div`
+const BodyContainerDesktop = styled.div`
   padding: 20px 16px 0px 16px;
 `;
 
-const Input = styled.input`
+const InputDesktop = styled.input`
   margin: 40px 25px;
   width: 200px;
   display: block;
@@ -143,13 +241,13 @@ const Body = ({ onChange, children }) => (
   </BodyContainer>
 );
 
-const FooterContainer = styled.div`
+const FooterContainerDesktop = styled.div`
   display: flex;
   justify-content: flex-end;
   padding: 2px 16px;
   color: white;
 `;
-const Button = styled.div`
+const ButtonDesktop = styled.div`
   margin: 5px;
   padding: 10px;
   color: ${props => props.color};
