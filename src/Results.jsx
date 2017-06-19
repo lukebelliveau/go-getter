dcbfe799a5caf63d9a0766064b146793b3dcec81
lukebelliveau <lbellive@thoughtworks.com>
2017-06-19 18:11:30 -0400

    split Results styles out

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,12 +1,11 @@
 // @flow
 import React from 'react';
-import styled from 'styled-components';
 
 import { LOADING_RESULTS } from './App';
-import styles from './styles/Results.style';
 import LoadingSpinner from './LoadingSpinner';
+import styles from './styles/Results.style';
 
-const { mobileDeviceWidth, primaryColor, hoverColor } = styles;
+const { Selection, CenteredText } = styles;
 
 const { CenterContainer } = styles;
 
@@ -16,13 +15,8 @@ const ResultsContainer = ({ results, onClick }: props) => (
   <CenterContainer>
     {
       results === LOADING_RESULTS
-        ?
-        <LoadingSpinner>Hey</LoadingSpinner>
-        :  results
-          ? <div id="searchResults" style={{ width: '100%' }}>
-              <ResultList results={ results } onClick={ onClick } />
-            </div>
-          : null
+        ? <LoadingSpinner />
+        : <ResultList results={ results } onClick={ onClick } />
     }
   </CenterContainer>
 );
@@ -31,55 +25,17 @@ export const ResultList = ({ results, onClick }: props) => (
   <CenterContainer>
     {
       results.length === 0
-        ? <NoResults>Sorry, the search for brands returned no results. Please try a different search.</NoResults>
+        ? <CenteredText>Sorry, the search for brands returned no results. Please try a different search.</CenteredText>
         : Object.keys(results).map(key => {
-        const brandName = results[key];
-        return(
-          <Button
-            onClick={ () => onClick(brandName) }
-            key={ brandName } >
-              { brandName }
-          </Button>
-        )
-      })
+            const brandName = results[key];
+            return (
+              <Selection onClick={ () => onClick(brandName) } key={ brandName }>
+                  { brandName }
+              </Selection>
+            )
+        })
     }
   </CenterContainer>
 );
-
-const NoResults = styled.div`
-  font-family: Roboto, sans-serif;
-  font-size: 50px;
-  text-align: center;
-  @media (min-device-width: ${mobileDeviceWidth}){
-    font-size: initial;
-    text-align: initial;
-  }
-`
-
-const Button = styled.div`
-  background-color: ${ primaryColor };
-  margin: 10px;
-  height: 125px;
-  width: 100%;
-  font-size: 50px;
-  color: #FFFFFF;
-  font-family: Roboto, sans-serif;
-  text-align: center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  
-  &:hover{
-    background-color: ${hoverColor};
-    cursor: pointer;
-  }
-  
-  @media (min-device-width: ${mobileDeviceWidth}){
-    height: 250px;
-    width: 250px;
-    font-size: 25px;
-    line-height: initial;
-  }
-`
 
 export default ResultsContainer;
--- src/styles/Results.style.js
+++ src/styles/Results.style.js
@@ -1,5 +1,45 @@
+import styled from 'styled-components';
+
 import shared from './shared';
+
+const Selection = styled.div`
+  background-color: ${shared.primaryColor};
+  margin: 10px;
+  height: 125px;
+  width: 100%;
+  font-size: 50px;
+  color: #FFFFFF;
+  font-family: Roboto, sans-serif;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  &:hover{
+    background-color: ${shared.hoverColor};
+    cursor: pointer;
+  }
+  
+  @media (min-device-width: ${shared.mobileDeviceWidth}){
+    height: 250px;
+    width: 250px;
+    font-size: 25px;
+    line-height: initial;
+  }
+`
+
+const CenteredText = styled.div`
+  font-family: Roboto, sans-serif;
+  font-size: 50px;
+  text-align: center;
+  @media (min-device-width: ${shared.mobileDeviceWidth}){
+    font-size: initial;
+    text-align: initial;
+  }
+`
 
 export default {
   ...shared,
+  Selection,
+  CenteredText,
 };
