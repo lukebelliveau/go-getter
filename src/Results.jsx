b43bef7740e61823718f97b47e809efa928855fe
lukebelliveau <lbellive@thoughtworks.com>
2017-06-19 12:17:23 -0400

    removed old code from material-ui implementation of Results

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { CircularProgress, RaisedButton, Text } from 'material-ui';
+import { CircularProgress } from 'material-ui';
 import styled from 'styled-components';
 
 import { LOADING_RESULTS } from './App';
-import ResponsiveContainer from './ResponsiveContainer';
 import styles from './styles';
 
 const { mobileDeviceWidth, primaryColor, hoverColor } = styles;
@@ -17,37 +16,12 @@ const ResultsContainer = ({ results, onClick }) => (
         ? <CircularProgress />
         :  results
           ? <div id="searchResults" style={{ width: '100%' }}>
-            {ResponsiveContainer({
-              mobileComponent: <ResultList results={ results } onClick={ onClick } />,
-              desktopComponent: <ResultList results={ results } onClick={ onClick } style={ desktopStyle }/>
-            })}
-          </div>
+              <ResultList results={ results } onClick={ onClick } />
+            </div>
           : null
     }
   </div>
 );
-
-// export const ResultList = ({ results, onClick, style }) => (
-//   <div style={ centerFlex }>
-//     {
-//       results.length === 0
-//         ? <div style={ style.noResults }>Sorry, the search for brands returned no results. Please try a different search.</div>
-//         : Object.keys(results).map(key => {
-//             const brandName = results[key];
-//             return(
-//               <RaisedButton
-//                 name={ brandName } onClick={ () => onClick(brandName) }
-//                 backgroundColor={ style.backgroundColor } style={ style.button }
-//                 key={ brandName } >
-//                   <div style={ style.brandName }>
-//                     { brandName }
-//                   </div>
-//               </RaisedButton>
-//             )
-//           })
-//     }
-//   </div>
-// );
 
 export const ResultList = ({ results, onClick, style }) => (
   <div style={ centerFlex }>
@@ -67,18 +41,6 @@ export const ResultList = ({ results, onClick, style }) => (
     }
   </div>
 );
-
-// const Button = styled.div`
-//   background-color: ${ primaryColor };
-//   margin: 10px;
-//   height: 125px;
-//   line-height: 125px;
-//   width: 100%;
-//   font-size: 50px;
-//   color: #FFFFFF;
-//   font-family: Roboto, sans-serif;
-//   text-align: center;
-// `
 
 const Button = styled.div`
   background-color: ${ primaryColor };
@@ -105,23 +67,5 @@ const Button = styled.div`
     line-height: initial;
   }
 `
-
-const backgroundColor = '#FA9100';
-const fontColor = '#FFFFFF';
-
-const desktopStyle = {
-  backgroundColor,
-  noResults: {
-    fontFamily: 'Roboto, sans-serif'
-  },
-  button: {
-    margin: 10,
-    height: 250,
-    width: 250,
-  },
-  brandName: {
-    color: fontColor,
-  }
-};
 
 export default ResultsContainer;
